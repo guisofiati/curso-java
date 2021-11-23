13ee333525a42ec53df6dd122d9bfbb034fbca81
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-11-23 06:37:17 -0300

    Data processing and casting

---
+++ data_processing_and_casting/src/Main.java
@@ -0,0 +1,61 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int a, b;
+		
+		a = 5;
+		b = 2 * a;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println();
+		System.out.println("************************");
+		
+		int c;
+		double d;
+		
+		c = 5;
+		d = 2 * c;
+		
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println();
+		System.out.println("************************");
+		
+		double x, y, Z, area;
+		
+		x = 6.0;
+		y = 5.0;
+		Z = 8.0;
+		
+		area = (x + Z) / 2 * y;
+		
+		System.out.println(area);
+		System.out.println();
+		System.out.println("************************");
+		
+		int e, f;
+		double resultado;
+		
+		e = 5;
+		f = 2;
+		
+		resultado = (double) e / f;
+		
+		System.out.println(resultado);
+		System.out.println();
+		System.out.println("************************");
+		
+		double g;
+		int h;
+		
+		g = 5.0;
+		h = (int) g;
+		
+		System.out.println(h);
+		System.out.println();
+		System.out.println("************************");
+	}
+}
