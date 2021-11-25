54b05ebd908d0476edc4946376655df03980323d
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-11-25 06:51:49 -0300

    Estrutura condicional e operadores

---
+++ estrutura_condicional/src/estrutura_condicional/Main.java
@@ -0,0 +1,36 @@
+package estrutura_condicional;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int hora;
+		
+		System.out.println("Que horas são?");
+		hora = sc.nextInt();
+
+//      Menos casual
+//		
+//		if (hora < 12) {
+//			System.out.println("Bom dia");
+//		} else {
+//			if (hora < 18) {
+//				System.out.println("Boa tarde");
+//			} else {
+//				System.out.println("Boa noite");
+//			}
+//		}
+		
+		if (hora < 12) {
+			System.out.println("Bom dia");
+		} else if (hora < 18) {
+			System.out.println("Boa tarde");
+		} else {
+			System.out.println("Boa noite");
+		}
+	}
+}
