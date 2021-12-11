ff9573cb58b9647a1ce33b3a6e37368dceda51f4
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-12-11 19:36:07 -0300

    Static members. Solution 2

--- statics/src/application/Program.java
+++ statics/src/application/Program.java
@@ -3,36 +3,29 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Program {
+import utils.Calculator;
 
-	// final quer dizer que será uma constante, não poderá mudar.
-	public static final double PI = 3.14159; 		
+public class Program {
 
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		//Tivemos que instanciar o objeto para chamar os métodos
+		Calculator calc = new Calculator();
+		
 		System.out.print("Enter radius: ");
 		double radius = sc.nextDouble();
 		
-		double c = circumference(radius);
+		double c = calc.circumference(radius);
 		
-		double v = volume(radius);
+		double v = calc.volume(radius);
 		
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("PI: %.2f%n", PI);
+		System.out.printf("PI: %.2f%n", calc.PI);
 		
 		sc.close();
-		
-	}
-
-	public static double circumference(double radius) {
-		return 2.0 * PI * radius;
-	}
-	
-	public static double volume(double radius) {
-		return 4.0 * PI * Math.pow(radius, 3) / 3.0;
 	}
 }
