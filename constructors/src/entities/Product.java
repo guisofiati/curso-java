b94e114d3d25cf6088133174557ff3add7f29d31
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-12-13 07:03:06 -0300

    Overload (constructors)

--- constructors/src/entities/Product.java
+++ constructors/src/entities/Product.java
@@ -6,10 +6,21 @@ public class Product {
 	public double price;
 	public int quantity;
 	
+	public Product() {
+	}
+	
 	public Product(String name, double price, int quantity) {
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
+	}
+	
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+		
+		// Por padrão já vai vir 0
+		//quantity = 0;
 	}
 	
 	public double totalValueInStock() {
