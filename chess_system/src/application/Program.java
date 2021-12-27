4f86650cf5c482399b0f601cbaec1645ae8b2810
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-12-27 06:44:14 -0300

    Chess: Position class

---
+++ chess_system/src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Position position = new Position(5, 10);
+		
+		System.out.println(position);
+
+	}
+}
